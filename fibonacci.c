7829dda05d36c9f6de651e8090391487edb86b5a
SilyQuestion <otbredbaron@gmail.com>
2019-11-12 03:10:40 +0100

    Update fibonacci.c
    Enforcing C99

--- fibonacci.c
+++ fibonacci.c
@@ -11,13 +11,12 @@ Fibonacci Sequence iterative in linear time.
 int main(void)
 {
     uint64_t a[2] = {0, 1};
-    size_t i;
 
     printf( "N°[0] = %" PRIu64 "\n"
             "N°[1] = %" PRIu64 "\n",
             a[0], a[1]);
 
-    for (i = 0; i < MAX_N - 1; ++i) {
+    for (size_t i = 0; i < MAX_N - 1; ++i) {
         uint64_t j = i & 1;
         a[j] += a[j ^ 1];
         printf("N°[%" PRIu64 "] = %" PRIu64 "\n", i + 2, a[j]);
